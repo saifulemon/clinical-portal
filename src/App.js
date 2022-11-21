ec61d585d78578c3d75496dbe6fa4a9f5b8eaaa4
saifulemon <463saiful0@gmail.com>
2022-11-21 09:15:42 +0600

    added header and Banner in home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Banner";
+import Login from "./Pages/Login/Login";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
-    <div>
-      <button className='btn btn-primary'>Hello primary</button>
-    </div>
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/appointment" ></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </>
   );
 }
 
