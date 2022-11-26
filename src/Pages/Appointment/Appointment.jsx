a24af6f5bb658baf597568f7d45a0841bcb76780
saifulemon <463saiful0@gmail.com>
2022-11-26 11:05:50 +0600

    added services.json file

--- src/Pages/Appointment/Appointment.jsx
+++ src/Pages/Appointment/Appointment.jsx
@@ -6,7 +6,7 @@ import AvailableAppoint from './AvailableAppoint';
 const Appointment = () => {
     const [date, setDate] = useState(new Date());
     return (
-        <div>
+        <div className="px-14">
             <AppointBanner date={date} setDate={setDate} />
             <AvailableAppoint date={date} />
             <Footer />
--- src/Pages/Appointment/AvailableAppoint.jsx
+++ src/Pages/Appointment/AvailableAppoint.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AvailableAppoint = ({date}) => {
     return (
         <div>
-            Available Appointment on : {format(date, 'PP')}
+            <h4 className='text-xl text-secondary text-center'>Available Appointment on : {format(date, 'PP')}</h4>
         </div>
     );
 };
