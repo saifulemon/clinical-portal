18b7955c218eaef7b2958fa66257fda88eea35e0
saifulemon <463saiful0@gmail.com>
2022-12-03 10:52:23 +0600

    changed loading effect

--- src/Pages/Shared/Loading/Loading.jsx
+++ src/Pages/Shared/Loading/Loading.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
-    return (
-        <div className='h-screen flex justify-center items-center'>
-            <button className="btn loading">loading</button>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="relative">
+        <div className="w-20 h-20 border-purple-200 border-2 rounded-full"></div>
+        <div className="w-20 h-20 border-purple-700 border-t-2 animate-spin rounded-full absolute left-0 top-0"></div>
+      </div>
+
+      <div className="relative">
+        <div className="w-10 h-10 border-purple-200 border-2 rounded-full"></div>
+        <div className="w-10 h-10 border-purple-700 border-t-2 animate-spin rounded-full absolute left-0 top-0"></div>
+      </div>
+
+      <div className="relative">
+        <div className="w-5 h-5 border-purple-200 border-2 rounded-full"></div>
+        <div className="w-5 h-5 border-purple-700 border-t-2 animate-spin rounded-full absolute left-0 top-0"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
